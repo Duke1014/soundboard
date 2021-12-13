fb7b1ba00fb32f988bdce1ecfd90ada57263bf2f
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-12-13 14:24:05 -0500

    added links to home

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <h1>Duke's Soundboard</h1>
+            <div>
+                <h3><Link to="/soundboard" className="soundboard">Soundboard</Link></h3>
+                <h3><Link to="/soundboard-creation" className="soundboard-creation">Make A Sound</Link></h3>
+                <h3><Link to="/about" className="about">About</Link></h3>
+            </div>
         </div>
     )
 }
