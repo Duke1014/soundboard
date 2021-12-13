61f716f93a6987adf94dd68e95f8ae5f3143dfcd
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-12-13 13:20:56 -0500

    added Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './Home';
+
 export default function App() {
   return (
     <BrowserRouter>
