4b5165b0b4edc8c056486991ef630335cf639204
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-12-13 17:55:38 -0500

    soundboard back button added

--- client/src/Soundboard.js
+++ client/src/Soundboard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Soundboard() {
+    
     return (
         <div>
-            
+            <div>You must be signed in to use the soundboard</div>
+            <br/>
+            <Link to="/" className="back-button">Back</Link>
         </div>
     )
 }
