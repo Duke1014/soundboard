a447759be32e947b04c27bba624f75c8f1fbe7c2
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-12-17 15:48:17 -0500

    soundboard initialized, now maps out sound boxes!

--- client/src/Soundboard.js
+++ client/src/Soundboard.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import SoundBox from './SoundBox'
 
 export default function Soundboard({  }) {
 
+    const [sounds, setSounds] = useState([])
+
     useEffect(() => {
-        fetch("/sounds").then((r) => {
-            console.log(r)
-        })
-    })
+        fetch("/sounds")
+        .then((r) => r.json())
+        .then(setSounds)
+    }, [])
 
     return (
         <div>
-
+            {sounds.length > 0 ? (
+                sounds.map((sound) => (
+                    <SoundBox key={sound.id} name={sound.name} description={sound.description} sound_url={sound.sound_url} />
+                ))
+            ) : <>
+                <div>No Sounds Found :(</div>
+            </>}
             <br/>
             <Link to="/" className="back-button">Back</Link>
         </div>
