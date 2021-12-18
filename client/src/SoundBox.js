186f79cdcdce46cf76b40b048446a33fd714b13b
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-12-18 01:44:13 -0500

    i did a very important button function yes

--- client/src/SoundBox.js
+++ client/src/SoundBox.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 export default function SoundBox({ key, name, description, sound_url }) {
 
+    const handlePlay = () => {
+        console.log("MOO")
+    }
 
     return (
         <div>
             <h3>{name}</h3>
             <h4>{description}</h4>
-            Play Button Here
+            <button onClick={handlePlay}>►</button>
             <br/>
             Edit Button Here
         </div>
